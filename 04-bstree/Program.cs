e095ff3cd2418161499b4b530e86043376adf653
Sipos Miklos <commit@siposm.hu>
2026-03-18 20:35:14 +0100

    bstree final

--- 04-bstree/Program.cs
+++ 04-bstree/Program.cs
@@ -1,9 +1,154 @@
 ﻿namespace _04_bstree;
 
+enum TraversalTypes
+{
+    PreOrder,
+    InOrder,
+    PostOrder,
+}
+
+class BST<K, T>
+    where K : IComparable
+{
+    private TreeItem? root = null;
+
+    class TreeItem
+    {
+        public K key;
+        public T content;
+        public TreeItem? left;
+        public TreeItem? right;
+
+        public TreeItem(K key, T content)
+        {
+            this.key = key;
+            this.content = content;
+            this.left = null;
+            this.right = null;
+        }
+
+        public override string ToString()
+        {
+            return $"{key.ToString()} - {content?.ToString()}";
+        }
+    }
+
+    #region insertion
+    public void Insert(K key, T content)
+    {
+        Insert(ref this.root!, key, content);
+    }
+
+    private void Insert(ref TreeItem p, K key, T content)
+    {
+        if (p == null)
+            p = new TreeItem(key, content);
+        else if (p.key.CompareTo(key) < 0)
+            Insert(ref p.right, key, content);
+        else if (p.key.CompareTo(key) > 0)
+            Insert(ref p.left, key, content);
+        else
+            throw new Exception("Item with the given key already exists.");
+    }
+    #endregion
+
+    #region  traversal
+    public void Traverse(TraversalTypes type, Action<T> action)
+    {
+        if (this.root == null)
+            return;
+
+        if (type == TraversalTypes.PreOrder)
+            PreOrder(this.root, action);
+        else if (type == TraversalTypes.InOrder)
+            InOrder(this.root, action);
+        else if (type == TraversalTypes.PostOrder)
+            PostOrder(this.root, action);
+    }
+
+    private void PreOrder(TreeItem? p, Action<T> action)
+    {
+        if (p != null)
+        {
+            action(p.content);
+            PreOrder(p.left, action!);
+            PreOrder(p.right, action!);
+        }
+    }
+
+    private void InOrder(TreeItem? p, Action<T> action)
+    {
+        if (p != null)
+        {
+            InOrder(p.left, action!);
+            action(p.content);
+            InOrder(p.right, action!);
+        }
+    }
+
+    private void PostOrder(TreeItem? p, Action<T> action)
+    {
+        if (p != null)
+        {
+            PostOrder(p.left, action!);
+            PostOrder(p.right, action!);
+            action(p.content);
+        }
+    }
+    #endregion
+
+    #region deletion
+    public void Delete(K key)
+    {
+        Delete(ref this.root, key);
+    }
+
+    private void Delete(ref TreeItem? p, K key)
+    {
+        if (p == null) throw new InvalidOperationException("Item with this key does not exist.");
+
+        int cmp = p.key.CompareTo(key);
+
+        if (cmp > 0) Delete(ref p.left, key);
+        else if (cmp < 0) Delete(ref p.right, key);
+        else
+        {
+            // megtaláltuk a törlendő csúcsot
+            if (p.left == null) p = p.right;
+            else if (p.right == null) p = p.left;
+            else DeleteWithTwoChildren(p, ref p.left);
+        }
+    }
+
+    private void DeleteWithTwoChildren(TreeItem target, ref TreeItem? r)
+    {
+        if (r == null) return;
+
+        if (r.right != null) DeleteWithTwoChildren(target, ref r.right);
+        else
+        {
+            target.key = r.key;
+            target.content = r.content;
+            r = r.left;
+        }
+    }
+    #endregion
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello BSTree!");
+        var bst = new BST<int, string>();
+        bst.Insert(20, "alma");
+        bst.Insert(10, "szilva");
+        bst.Insert(30, "körte");
+        bst.Insert(22, "barack");
+        bst.Insert(31, "narancs");
+        bst.Insert(9, "eper");
+
+        bst.Delete(30);
+        ;
     }
 }
---
+++ homework/04-bstree.md
@@ -0,0 +1,51 @@
+## Házi feladat — Bináris keresőfa
+
+Készítsen generikus bináris keresőfát a tanultaknak megfelelően majd a következő feladatokat implementálja rajta.
+
+### 1) Statiksztikák
+
+Készítsen egy `TreeStatistics` osztályt, amely metódusai megkapják a generikus bináris keresőfákat, és a következőket határozzák meg:
+
+- fa magasság: mi a fa magassága
+- csúcsok száma: hány csúcsa van
+- levelek száma: hány levele van
+- min: mi a legkisebb elem értéke
+- max: mi a legnagyobb elem értéke
+
+### 2) Tömb rendezés
+
+Generáljon egy tetszőleges tömböt, amelyből építsen fel bináris keresőfát, majd a fát a megfelelő bejárással feldolgozva adja vissza rendezetten az elemeket az eredeti tömbbe.
+
+### 3) Szótár alkalmazás
+
+Készítsen egy olyan konzolos alkalmazást, amely egy **angol–magyar szótárat valósít meg bináris keresőfa segítségével**. A program minden szótári bejegyzésnél tárolja az angol szót és a hozzá tartozó magyar fordítást. A bináris keresőfában az elemeket az angol szó szerint kell elhelyezni.
+
+Feladatok:
+
+- Készítsen metódust, amely adott kezdőbetű szerint listázza a szópárokat.
+- Lehessen egy adott angol szóhoz tartozó magyar fordítást frissíteni.
+- Lehessen csak a magyar vagy csak az angol szavakat listázni.
+- JSON fájlba szerializálja ki a szótár tartalmát a lentebbi mintának megfelelően:
+
+```json
+[
+    { "eng": "apple", "hun": "alma"   },
+    { "eng": "cat",   "hun": "macska" }
+]
+```
+
+### 4) ESport rangsor
+
+Készítsen **rendezett láncolt lista** és **bináris keresőfa** segítségével egy csapatok eltárolására alkalmas rendszert. Mind a két adatszerkezetet **generikusan** valósítsa meg.
+
+A csapatok résztvevőit `Player` objektumokkal képezze le a következő tulajdonságokkal:
+
+- `id`: a játékos azonosítója
+- `name`: a játékos neve
+- `score`: a játékos pontszáma
+
+A csapatokat listával képezze le, ahol a játékosok pontszáma alapján történjen a beszúrás logikája.
+
+Az összeállított csapatokat bináris keresőfában tárolja el. Minden csapatnak készítsen egy algoritmus alapján valamilyen egyedi azonosítót, ami alapján a fába való beszúrás megtörténik.
+
+Járja be a fát és írja ki a csapatokat a konzolra.
