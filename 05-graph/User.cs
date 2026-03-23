2bbe8c8c50d3d1e0ae3c9cd4807cffdd142ed931
Sipos Miklos <commit@siposm.hu>
2026-03-23 12:55:44 +0100

    user class updated

--- 05-graph/User.cs
+++ 05-graph/User.cs
@@ -2,10 +2,23 @@ namespace _05_graph;
 
 class User
 {
-    public string Name { get; set; } = "";
+    public string Name { get; set; } = "_anonymous_";
 
     public override string ToString()
     {
         return this.Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not User other)
+            return false;
+
+        return Name == other.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
     }
 }
